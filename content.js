de62a9fcaad2ab3c9af19011cdf03c3a9406dd97
justin <justin.chang.tw@outlook.com>
2021-03-09 14:02:48 +0800

    詞性轉換為中文發音

--- content.js
+++ content.js
@@ -1,3 +1,58 @@
+let partOfSpeech = [{
+    code: "n",
+    description: "名詞"
+}, {
+    code: "v",
+    description: "動詞"
+}, {
+    code: "vi",
+    description: "不及物動詞"
+}, {
+    code: "vt",
+    description: "及物動詞"
+}, {
+    code: "adj",
+    description: "形容詞"
+}, {
+    code: "adv",
+    description: "副詞"
+}, {
+    code: "conj",
+    description: "連接詞"
+}, {
+    code: "prep",
+    description: "介係詞"
+}, {
+    code: "pron",
+    description: "代名詞"
+}, {
+    code: "s",
+    description: "主詞"
+}, {
+    code: "sc",
+    description: "主詞補語"
+}, {
+    code: "o",
+    description: "受詞"
+}, {
+    code: "oc",
+    description: "受詞補語"
+}, {
+    code: "art",
+    description: "冠詞"
+}, {
+    code: "num",
+    description: "數詞"
+}, {
+    code: "int",
+    description: "感嘆詞"
+}, {
+    code: "u",
+    description: "不可數名詞"
+}, {
+    code: "c",
+    description: "可數名詞"
+}];
 chrome.storage.sync.get("wordBook", async({ wordBook }) => {
     sliderWords(wordBook)
 });
@@ -27,8 +82,10 @@ function setWordText(dom, dataList, index) {
     speak.addEventListener('click', function() {
         //使用原生JS
         setTimeout(function() { speech(dataList[index].word) }, 0)
-        setTimeout(function() { speech(dataList[index].description) }, 2000)
-    })
+        setTimeout(function() {
+            speech(transferCode(dataList[index].description))
+        }, 2000)
+    });
     dom.appendChild(speak);
     return speak;
 }
@@ -41,4 +98,12 @@ function speech(text) {
     var voiceName = "Google 國語（臺灣）"; //"Microsoft Hanhan Desktop - Chinese(Taiwan)" //微軟發音
     words.voice = voices.find(x => x.name == voiceName) || void(words.lang = "zh-TW"); //使用其它來源 發音
     window.speechSynthesis.speak(words);
+}
+
+function transferCode(text) {
+    partOfSpeech.forEach(x => {
+        var re = new RegExp(x.code, 'gi')
+        text = text.replace(re, x.description);
+    })
+    return text;
 }
